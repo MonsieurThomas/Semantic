bdb46cc5c888a0b552b10d97976bdaf36a20af4f
Thomas Romain <r.thomas06200@gmail.con>
2024-03-25 18:12:01 +0100

    fix build

--- pages/CanvasDrawing.tsx
+++ pages/CanvasDrawing.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import nestedObjectData from "../src/app/utils/NestedObject";
 import TotalItemsCounting from "./MapLogic/TotalItemsCounting";
 import countNestedObjectLevels from "./MapLogic/NestedObjectItemCounting";
-import AddCoordinates from "./MapLogic/AddCoordinates";
+// import AddCoordinates from "./MapLogic/AddCoordinates";
 import exploreObject from "./MapLogic/Explore";
 
 interface NestedObject {
@@ -12,12 +12,12 @@ interface NestedObject {
 }
 
 const CanvasDrawing = () => {
-  const nestedObject: any = countNestedObjectLevels(nestedObjectData);
+  // const nestedObject: any = countNestedObjectLevels(nestedObjectData);
   const total = TotalItemsCounting(nestedObjectData);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const objectWithCoordinate = AddCoordinates(nestedObjectData);
+  // const objectWithCoordinate = AddCoordinates(nestedObjectData);
 
-  exploreObject(nestedObjectData); 
+  exploreObject(nestedObjectData);
 
   const drawLevel = (
     ctx: CanvasRenderingContext2D,
@@ -35,51 +35,54 @@ const CanvasDrawing = () => {
     }
   };
 
-  const drawRectangles = (
-    ctx: CanvasRenderingContext2D,
-    obj: NestedObject,
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    depth: number
-  ) => {
-    if ("length" in obj) {
-      drawLevel(ctx, obj.length, x, y, width, height);
-      x += height + 20; // Decalage Niveau suivant
-    }
-
-    for (const key in obj) {
-      if (key !== "length" && typeof obj[key] === "object") {
-        drawRectangles(
-          ctx,
-          obj[key],
-          x + (width + 40), // ecartement entre niveaux
-          y,
-          width,
-          height,
-          depth + 1
-        );
+  const drawRectangles = useCallback(
+    (
+      ctx: CanvasRenderingContext2D,
+      obj: NestedObject,
+      x: number,
+      y: number,
+      width: number,
+      height: number,
+      depth: number
+    ) => {
+      if ("length" in obj) {
+        drawLevel(ctx, obj.length, x, y, width, height);
+        x += height + 20; // Decalage Niveau suivant
       }
-    }
-  };
+
+      for (const key in obj) {
+        if (key !== "length" && typeof obj[key] === "object") {
+          drawRectangles(
+            ctx,
+            obj[key],
+            x + (width + 40), // ecartement entre niveaux
+            y,
+            width,
+            height,
+            depth + 1
+          );
+        }
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     if (canvasRef.current) {
       const ctx = canvasRef.current.getContext("2d");
       if (ctx) {
         ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-        drawRectangles(ctx, nestedObject, 20, 20, 180, 80, 0);
+        drawRectangles(ctx, nestedObjectData, 20, 20, 180, 80, 0);
       }
     }
-  }, []);
+  }, [drawRectangles]); // data
 
   return (
     <div>
       <canvas ref={canvasRef} width="3000" height="500" />;
       <h1>This is input {JSON.stringify(nestedObjectData)}</h1>
       <div style={{ padding: "30px" }} />
-      <h1>This is countNestedObjectLevels {JSON.stringify(nestedObject)}</h1>
+      {/* <h1>This is countNestedObjectLevels {JSON.stringify(nestedObject)}</h1> */}
       <div style={{ padding: "30px" }} />
       <h1>
         This is TotalItemsCounting, total des extremités {JSON.stringify(total)}
@@ -87,7 +90,7 @@ const CanvasDrawing = () => {
       <div style={{ padding: "10px" }} />
       <h1>
         This is AddCoordinates, lapos;objet avec x,y et isParsed{" "}
-        {JSON.stringify(objectWithCoordinate)}
+        {/* {JSON.stringify(objectWithCoordinate)} */}
       </h1>
     </div>
   );
--- pages/MapLogic/NestedObjectItemCounting.tsx
+++ pages/MapLogic/NestedObjectItemCounting.tsx
@@ -3,34 +3,40 @@ interface NestedObject {
 }
 
 function countNestedObjectLevels(obj: NestedObject) {
-  const count = (currentObj: any, depth: number = 0) => {
-    if (typeof currentObj !== "object" || currentObj === null) {
-      return { value: currentObj, depth }; // For non-objects, return the value and depth directly
-    }
+  // const count = (currentObj: any, depth: number = 0) => {
+  //   if (typeof currentObj !== "object" || currentObj === null) {
+  //     return { value: currentObj, depth }; // For non-objects, return the value and depth directly
+  //   }
 
-    let structure: NestedObject = {};
-    let length = 0; // Initialize length count for this level
+  //   let structure: NestedObject = {};
+  //   let length = 0; // Initialize length count for this level
 
-    for (const key in currentObj) {
-      if (Object.prototype.hasOwnProperty.call(currentObj, key)) {
-        const item = currentObj[key];
-        const { structure: nestedStructure, depth: nestedDepth } = count(
-          item,
-          depth + 1
-        );
-        structure[key] = nestedStructure; // Add the nested structure or value directly
-        length += 1; // Count each item at this level
-      }
-    }
+  //   for (const key in currentObj) {
+  //     if (Object.prototype.hasOwnProperty.call(currentObj, key)) {
+  //       const item = currentObj[key];
+  //       const { structure: nestedStructure, depth: nestedDepth } = count(
+  //         item,
+  //         depth + 1
+  //       );
+  //       structure[key] = nestedStructure; // Add the nested structure or value directly
+  //       length += 1; // Count each item at this level
+  //     }
+  //   }
 
-    // Attach a length property and the nested structure or value
-    return { structure: { ...structure, length }, depth };
-    // return { structure: { ...structure, length }, depth };
-  };
+  //   // Attach a length property and the nested structure or value
+  //   return { structure: { ...structure, length }, depth };
+  //   // return { structure: { ...structure, length }, depth };
+  // };
 
-  const { structure: resultStructure } = count(obj);
-  return resultStructure;
+  // const { structure: resultStructure } = count(obj);
+  // return resultStructure;
 }
+
+
+
+
+
+
 
 //   const countNestedObjectLevels = (obj: NestedObject): Array<{level: number, count: number}> => {
 //     // Initialisation d'un tableau pour stocker les résultats
