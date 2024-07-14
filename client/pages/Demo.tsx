88e553333d58e885b677199e80ea773350e1ca4b
Thomas Romain <r.thomas06200@gmail.con>
2024-07-14 12:25:46 +0200

    fix demo

--- client/pages/Demo.tsx
+++ client/pages/Demo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import LoupeDemo from "../public/loupeDemo.png";
 import EyeDemo from "../public/EyeDemo.png";
@@ -8,14 +8,14 @@ function Demo() {
   const [docsPerWeek, setDocsPerWeek] = useState(0);
   const [pagesPerDoc, setPagesPerDoc] = useState(0);
 
-  const [email, setEmail] = useState('');
-  const [location, setLocation] = useState('');
-  const [companySize, setCompanySize] = useState('');
+  const [email, setEmail] = useState("");
+  const [location, setLocation] = useState("");
+  const [companySize, setCompanySize] = useState("");
 
   const handleCancel = () => {
-    setEmail('');
-    setLocation('');
-    setCompanySize('');
+    setEmail("");
+    setLocation("");
+    setCompanySize("");
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -28,21 +28,21 @@ function Demo() {
     };
 
     try {
-      const response = await fetch('/api/sendEmail', {
-        method: 'POST',
+      const response = await fetch("/api/sendEmail", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
 
       if (response.ok) {
-        console.log('Email sent successfully');
+        console.log("Email sent successfully");
       } else {
-        console.error('Error sending email');
+        console.error("Error sending email");
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
@@ -57,6 +57,21 @@ function Demo() {
   const calculateEarnings = () => {
     return (1 * pagesPerDoc * docsPerWeek) / 60;
   };
+
+  useEffect(() => {
+    // Create a script element
+    const script = document.createElement("script");
+    script.src = "https://cdn.reservio.com/widget/button-bundle.js";
+    script.async = true;
+
+    // Append the script element to the body
+    document.body.appendChild(script);
+
+    // Clean up the script element when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div className="flex mx-[100px] my-[0px] h-[115vh] gap-[2px]">
@@ -81,7 +96,7 @@ function Demo() {
           <Image
             src={EyeDemo}
             alt="Eye Demo"
-            className="w-[60px] mx-auto mt-6 "
+            className="w-[60px] mx-auto mt-4 mb-2 "
           />
           <h1 className="text-lg font-bold ">
             Structure ta pensée en un rien de temps
@@ -147,91 +162,104 @@ function Demo() {
         <h1 className="text-2xl font-bold mb-8">
           Réserve une démo de 30mn, gratuite et sans engagement :
         </h1>
-        <form className="space-y-4 mx-[50px] bg-white p-10" onSubmit={handleSubmit}>
-        <div>
+        <form
+          className="space-y-4 mx-[50px] bg-white p-10"
+          onSubmit={handleSubmit}
+        >
+          <div>
             <label className="block text-sm font-medium mb-2" htmlFor="email">
-            What is your email address?
+              What is your email address?
             </label>
             <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full border border-gray-300 rounded-md p-1 mb-3"
-            required
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full border border-gray-300 rounded-md p-1 mb-3"
+              required
             />
-        </div>
-        <div>
-            <label className="block text-sm font-medium my-2" htmlFor="location">
-            Where is your company located?
+          </div>
+          <div>
+            <label
+              className="block text-sm font-medium my-2"
+              htmlFor="location"
+            >
+              Where is your company located?
             </label>
             <input
-            type="text"
-            id="location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            className="w-full border border-gray-300 rounded-md p-1"
-            required
+              type="text"
+              id="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              className="w-full border border-gray-300 rounded-md p-1"
+              required
             />
-        </div>
-        <div>
+          </div>
+          <div>
             <label className="block text-lg font-medium mb-2">
-            What is the size of your company?
+              What is the size of your company?
             </label>
             <div className="flex flex-col space-y-2">
-            <label className="inline-flex items-center">
+              <label className="inline-flex items-center">
                 <input
-                type="radio"
-                name="companySize"
-                value="0-250"
-                checked={companySize === '0-250'}
-                onChange={(e) => setCompanySize(e.target.value)}
-                className="form-radio"
-                required
+                  type="radio"
+                  name="companySize"
+                  value="0-250"
+                  checked={companySize === "0-250"}
+                  onChange={(e) => setCompanySize(e.target.value)}
+                  className="form-radio"
+                  required
                 />
                 <span className="ml-2">0-250 employees</span>
-            </label>
-            <label className="inline-flex items-center">
+              </label>
+              <label className="inline-flex items-center">
                 <input
-                type="radio"
-                name="companySize"
-                value="250-500"
-                checked={companySize === '250-500'}
-                onChange={(e) => setCompanySize(e.target.value)}
-                className="form-radio"
-                required
+                  type="radio"
+                  name="companySize"
+                  value="250-500"
+                  checked={companySize === "250-500"}
+                  onChange={(e) => setCompanySize(e.target.value)}
+                  className="form-radio"
+                  required
                 />
                 <span className="ml-2">250-500 employees</span>
-            </label>
-            <label className="inline-flex items-center">
+              </label>
+              <label className="inline-flex items-center">
                 <input
-                type="radio"
-                name="companySize"
-                value="500+"
-                checked={companySize === '500+'}
-                onChange={(e) => setCompanySize(e.target.value)}
-                className="form-radio"
-                required
+                  type="radio"
+                  name="companySize"
+                  value="500+"
+                  checked={companySize === "500+"}
+                  onChange={(e) => setCompanySize(e.target.value)}
+                  className="form-radio"
+                  required
                 />
                 <span className="ml-2">500+ employees</span>
-            </label>
+              </label>
             </div>
-        </div>
-        <div className="flex space-x-4">
-            <button
-            type="button"
-            onClick={handleCancel}
-            className="flex-1 bg-gray-500 text-white py-2 rounded-md hover:bg-gray-600"
+          </div>
+          <div className="flex space-x-4">
+            <div
+              className="reservio-button-container"
+              data-text="Réserver maintenant"
+              data-size="large"
+              data-color="#FCA311"
+              data-url="https://semantic.reservio.com"
+            ></div>
+            {/* <button
+              type="button"
+              onClick={handleCancel}
+              className="flex-1 bg-gray-500 text-white py-2 rounded-md hover:bg-gray-600"
             >
-            Cancel
+              Cancel
             </button>
             <button
-            type="submit"
-            className="flex-1 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
+              type="submit"
+              className="flex-1 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
             >
-            Submit
-            </button>
-        </div>
+              Submit
+            </button> */}
+          </div>
         </form>
       </div>
     </div>
