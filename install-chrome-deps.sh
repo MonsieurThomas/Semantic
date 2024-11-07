fc316006b0a711fd8e5449cfca6e21d3cfe4b266
Thomas Romain <r.thomas06200@gmail.con>
2024-11-07 14:03:00 +0100

    degux url

--- install-chrome-deps.sh
+++ install-chrome-deps.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+set -x
 
 echo "script Updating package lists..."
 apt-get update && apt-get install -y wget gnupg
 
 echo "script Installing required libraries..."
 apt-get install -y libxss1 libasound2 libnss3 libx11-xcb1 libatk-bridge2.0-0 libgtk-3-0
-
